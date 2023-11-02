a35d4cacbd6fff7141777fea4edecc716ab4c739
IlsevNierop <ilsevannierop@gmail.com>
2023-11-02 17:07:00 +0100

    add geotracking to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [speed, setSpeed] = useState(0);
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition(function(position) {
+      const {latitude, longitude, speed} = position.coords;
+      setLongitude(longitude);
+      setLatitude(latitude);
+      setSpeed(speed);
+      console.log(longitude)
+      console.log(latitude)
+      console.log(speed)
+    }, function (error) {
+      console.error("Something is wrong: " + error)
+    }, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
+  }
+  else {
+    console.log("Geolocation is not supported with this browser.")
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Test gps</h1>
+          <p>Longitude {longitude}</p>
+          <p>Latitude {latitude}</p>
+          <p>Speed {speed}</p>
+        </header>
+
+        {/*<div id="map"></div>*/}
+
+
+      </div>
   );
 }
 
